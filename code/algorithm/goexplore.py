0a811e795da25a2dc789f9d0bdd11d2b443c688c
Patrik <patrik95195@gmail.com>
2022-02-24 15:54:01 +0100

    Implement linked actions

--- code/algorithm/goexplore.py
+++ code/algorithm/goexplore.py
@@ -51,20 +51,19 @@ class GoExplore:
         # self.highscore, self.n_frames, self.n_episodes = 0, 0, 0
         scores, n_cells, updates, discoveries = [], [], [], []
         while (self.n_frames < self.max_frames):
+            iter_start = time.time()
             # Sample cell from archive
-            # print('START WHILE')
             cell = self.cell_selector.sample()
-            # print('WHILE:', cell)
             self._explore_from(cell)
-            # print()
-            # self.highscore, self.n_frames = self._explore(
-            #     cell, archive, self.highscore, self.n_frames, updates, discoveries)
 
             self.n_episodes += 1
 
             # Track for logging
             scores.append(self.highscore)
             n_cells.append(len(self.archive))
+            iter_end = time.time()
+            print(
+                f'Processed frames: {self.n_frames}/{self.max_frames} Iteration time: {round(iter_end - iter_start, 3)}s', end='\r')
 
         # Extract cell that reached terminal state with highest score and smallest trajectory
         # cells = list(archive.values())
@@ -78,27 +77,23 @@ class GoExplore:
         names = ['env', 'highscore', 'duration', 'episodes', 'n_frames',
                  'trajectory', 'scores', 'n_cells', 'discoveries', 'updates']
         values = [self.env.unwrapped.spec.id, self.highscore, str(timedelta(seconds=duration)), self.n_episodes,
-                  self.n_frames, best_cell.trajectory, scores, n_cells, discoveries, updates]
+                  self.n_frames, best_cell.get_trajectory(), scores, n_cells, discoveries, updates]
         self.logger.add(names, values)
         self.logger.save()
 
     # def _explore_from(self, cell, archive, self.highscore, self.n_frames, updates, discoveries):
     def _explore_from(self, cell):
-        # print('EXPLORE:', cell)
         # Restore to cell's simulator state and retrieve its score and trajectory
-        # self.env.reset()
-        cell.restore_state(self.env)
-        trajectory, score = cell.history()
+        latest_action, traj_len, score = cell.load(self.env)
 
         # Termination criteria
         is_terminal = False
         n_steps = 0
         MAX_STEPS = 100
 
-        # Only increment visits at most once per exploration iteration
+        # Maintain seen set to only increment visits at most once per exploration iteration
         cells_seen_during_iteration = set()
 
-        # n_updates, n_discoveries = 0, 0
         while (n_steps < MAX_STEPS and not is_terminal):
             # Interact
             action = self.agent.act()
@@ -107,16 +102,33 @@ class GoExplore:
 
             # Track trajectory and score in case cells during current iteration need to be
             # updated or created
-            trajectory.append(action)
+            latest_action = Action(action, prev=latest_action)
+            traj_len += 1
             score += reward
 
-            # Create cell if it is not in the archive. Update the cell if it has improved
-            # performance, i.e. if it has a higher score or if it has the same score but
-            # a shorter trajectory. A third alternative is that the current cell is not
-            # a new cell nor is it a better cell, in which case nothing is done.
-            cell = self._update_or_create_cell(
-                self.archive, cell_representation, score, deepcopy(trajectory))
-            # self.archive, cell_representation, score, deepcopy(trajectory), n_updates, n_discoveries)
+            # Create cell if it is not in the archive. Update the cell if it is in the archive,
+            # but has improved performance, i.e. if it has a higher score or if it has the same
+            # score but a shorter trajectory. A third alternative is that the current cell is
+            # not a new cell nor is it a better cell, in which case nothing is done.
+            if cell_representation not in self.archive:
+                simulator_state = self.env.unwrapped.clone_state(
+                    include_rng=True)
+                cell = Cell(simulator_state, latest_action, traj_len, score)
+                self.cell_selector.add(cell)
+                self.archive[cell_representation] = self.cell_index
+                self.cell_index += 1
+            else:
+                # Get cell from archive and compare scores/traj_lens with current cell
+                cell_index = self.archive[cell_representation]
+                cell = self.cell_selector.cells[cell_index]
+
+                if cell.is_worse(score, traj_len):
+                    simulator_state = self.env.unwrapped.clone_state(
+                        include_rng=True)
+                    cell.update(simulator_state,
+                                latest_action, traj_len, score)
+                    self.cell_selector.update_weight(cell_index, cell.visits)
+                # TODO: Do I need to clear action links from memory if the cell is not updated?
 
             # Increment visit count/update weights if cell not seen during the episode
             if cell_representation not in cells_seen_during_iteration:
@@ -127,54 +139,28 @@ class GoExplore:
 
             if score > self.highscore:
                 self.highscore = score
-                print(f'New highscore: {self.highscore}')
+                # print(f'New highscore: {self.highscore}')
 
             n_steps += 1
             self.n_frames += 1
             if is_terminal:
                 cell.set_done()
                 break
-            if self.verbose and self.n_frames % 50000 == 0:
-                print(
-                    f'Frames: {self.n_frames}\tScore: {self.highscore}\t Cells: {len(self.archive)}')
-        # updates.append(n_updates)
-        # discoveries.append(n_discoveries)
-
-        # TODO: self.frame_update(), self.iteration_update()
-
-        # return highscore, self.n_frames
-
-    def _update_or_create_cell(self, archive, cell_representation, score, trajectory):
-        # def _update_or_create_cell(self, archive, cell_representation, score, trajectory, n_updates, n_discoveries):
-        if cell_representation in archive:
-            # cell = archive[cell_representation]
-            cell_index = archive[cell_representation]
-            cell = self.cell_selector.cells[cell_index]
-            if cell.is_worse(score, len(trajectory)):
-                simulator_state = self.env.unwrapped.clone_state(
-                    include_rng=True)
-                cell.update(simulator_state, trajectory, score)
-                self.cell_selector.update_weight(cell_index, cell.visits)
-                # n_updates += 1
-        else:
-            simulator_state = self.env.unwrapped.clone_state(include_rng=True)
-            cell = Cell(simulator_state, trajectory, score)
-            self.cell_selector.add(cell)
-            archive[cell_representation] = self.cell_index
-            self.cell_index += 1
-            # n_discoveries += 1
-        return cell
+            # if self.verbose and self.n_frames % 50000 == 0:
+            #     print(
+            #         f'Frames: {self.n_frames}\tScore: {self.highscore}\t Cells: {len(self.archive)}')
 
 
 class Cell:
-    def __init__(self, simulator_state, trajectory=[], score=0.0):
+    def __init__(self, simulator_state, latest_action=None, traj_len=0, score=0.0):
         self.visits = 1
         self.done = False
-        self.update(simulator_state, trajectory, score)
+        self.update(simulator_state, latest_action, traj_len, score)
 
-    def update(self, simulator_state, trajectory, score):
+    def update(self, simulator_state, latest_action, traj_len, score):
         self.simulator_state = simulator_state
-        self.trajectory = trajectory
+        self.latest_action = latest_action
+        self.traj_len = traj_len
         self.score = score
 
     def increment_visits(self):
@@ -183,25 +169,40 @@ class Cell:
     def get_weight(self):
         return 1 / np.log(self.visits + 1)
 
-    def restore_state(self, env):
+    def load(self, env):
         env.unwrapped.restore_state(self.simulator_state)
+        return self.latest_action, self.traj_len, self.score
 
-    def history(self):
-        return deepcopy(self.trajectory), deepcopy(self.score)
-
-    def is_worse(self, score, actions_taken_length):
+    def is_worse(self, score, traj_len):
         return ((score > self.score)
-                or (score == self.score and actions_taken_length < len(self.trajectory)))
+                or (score == self.score and traj_len < self.traj_len))
 
     def set_done(self):
         self.done = True
 
+    def get_trajectory(self):
+        actions = []
+        a = self.latest_action
+        while a:
+            actions = [a.action] + actions  # Prepend previous actions
+            a = a.prev
+        return actions
+
     def __repr__(self):
-        return f'Cell(score={self.score}, traj_len={len(self.trajectory)}, visits={self.visits}, done={self.done})'
+        return f'Cell(score={self.score}, traj_len={self.traj_len}, visits={self.visits}, done={self.done})'
 
     # Make sortable
     def __eq__(self, other):
         return self.score == other.score and self.lenght == other.length
 
     def __lt__(self, other):
-        return (-self.score, len(self.trajectory)) < (-other.score, len(other.trajectory))
+        return (-self.score, self.traj_len) < (-other.score, self.traj_len)
+
+
+class Action:
+    def __init__(self, action, prev=None):
+        self.prev = prev
+        self.action = action
+
+    def __repr__(self):
+        return str(self.action)
